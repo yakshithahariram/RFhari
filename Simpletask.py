303e8949d92a5f8db9cfdc9b30ef456985688497
yakshithahariram <haritharamreddy111@gmail.com>
2020-11-25 14:46:19 +1100

    Create Simpletask.py

---
+++ Simpletask.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+def main():
+    portlist = ["32769", "32770","32771","32782"]
+    ports_up = []
+    ports_down = []
+    i = 0
+    while(i<4):
+       z = portlist[i]+" is up"
+       if z in open("/home/vagrant/venv/rfsample/RFhari/output.xml").read():
+          ports_up.append(portlist[i])
+          i = i+1
+       else:
+          ports_down.append(portlist[i])
+          i=i+1
+   # print("ports_up :",ports_up)
+    print("Unavailable ports from EVE-NG:",ports_down)
+if __name__ == '__main__':
+    main()
